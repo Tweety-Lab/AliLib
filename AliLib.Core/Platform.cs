95651d970838a79ebdfa8d8e716c75cb2749eff7
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2026-04-06 01:28:43 +1000

    Add `Platform.DumpInfo`.

--- AliLib.Core/Platform.cs
+++ AliLib.Core/Platform.cs
@@ -24,4 +24,21 @@ public static class Platform
             return Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.OSXPlayer;
         }
     }
+
+    /// <summary> Dumps info about the current platform to the console. </summary>
+    /// <returns> The dumped string. </returns>
+    public static string DumpInfo()
+    {
+        // StringBuilder is a bit overkill here but it scales
+        var sb = new System.Text.StringBuilder();
+
+        sb.AppendLine($"IsNomad: {IsNomad}");
+        sb.AppendLine($"IsPCVR: {IsPCVR}");
+        sb.AppendLine($"Application.platform: {Application.platform}");
+        sb.AppendLine($"Application.systemLanguage: {Application.systemLanguage}");
+
+        string dump = sb.ToString();
+        Debug.Log(dump);
+        return dump;
+    }
 }
