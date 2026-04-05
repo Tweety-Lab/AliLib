5086e7c55c44e1e43e749c3c8ea0e158e93a7680
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2026-04-06 01:21:52 +1000

    Add `AliLib.Analyzer`.

---
+++ AliLib.Analyzer/DiagnosticRules/NullableAddressablesAnalyzer.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace AliLib.Analyzer.DiagnosticRules
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class NullableAddressablesAnalyzer : DiagnosticAnalyzer
+    {
+        public static DiagnosticDescriptor Rule { get; } = new DiagnosticDescriptor(
+            id: "ALI0001",
+            title: "Nullable Addressables",
+            messageFormat: "Addressable asset '{0}' should be nullable.",
+            category: "Usage",
+            defaultSeverity: DiagnosticSeverity.Warning,
+            isEnabledByDefault: true,
+            description: "Properties marked with [Addressable] should be nullable due to assets being loaded asynchronously."
+            );
+
+        /// <inheritdoc/>
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        /// <inheritdoc/>
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.RegisterSyntaxNodeAction(Analyze, SyntaxKind.PropertyDeclaration);
+        }
+
+        private static void Analyze(SyntaxNodeAnalysisContext context)
+        {
+            var property = (PropertyDeclarationSyntax)context.Node;
+            var symbol = context.SemanticModel.GetDeclaredSymbol(property);
+
+            if (symbol is null)
+                return;
+
+            bool hasAttribute = symbol.GetAttributes().Any(a => a.AttributeClass?.Name == "AddressableAttribute");
+
+            if (!hasAttribute)
+                return;
+
+            bool isNullable = symbol.Type.NullableAnnotation == NullableAnnotation.Annotated;
+
+            if (!isNullable)
+                context.ReportDiagnostic(Diagnostic.Create(Rule, property.GetLocation(), symbol.Name));
+        }
+    }
+
+}
---
+++ AliLib.Analyzer/DiagnosticRules/StaticAddressablesAnalyzer.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace AliLib.Analyzer.DiagnosticRules
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class StaticAddressablesAnalyzer : DiagnosticAnalyzer
+    {
+        public static DiagnosticDescriptor Rule { get; } = new DiagnosticDescriptor(
+            id: "ALI0002",
+            title: "Static Addressables",
+            messageFormat: "Addressable asset '{0}' must be static.",
+            category: "Usage",
+            defaultSeverity: DiagnosticSeverity.Error,
+            isEnabledByDefault: true,
+            description: "Properties marked with [Addressable] must be static to be loaded correctly."
+            );
+
+        /// <inheritdoc/>
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        /// <inheritdoc/>
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.RegisterSyntaxNodeAction(Analyze, SyntaxKind.PropertyDeclaration);
+        }
+
+        private static void Analyze(SyntaxNodeAnalysisContext context)
+        {
+            var property = (PropertyDeclarationSyntax)context.Node;
+            var symbol = context.SemanticModel.GetDeclaredSymbol(property);
+
+            if (symbol is null)
+                return;
+
+            bool hasAttribute = symbol.GetAttributes().Any(a => a.AttributeClass?.Name == "AddressableAttribute");
+
+            if (!hasAttribute)
+                return;
+
+            if (!symbol.IsStatic)
+                context.ReportDiagnostic(Diagnostic.Create(Rule, property.GetLocation(), symbol.Name));
+        }
+    }
+
+}
