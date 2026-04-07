a04bced17021c5a297c28c09f8950cd146a9af67
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2026-04-07 22:32:13 +1000

    Guard against double initialization.

--- AliLib.Core/AliLib.CoreScript.cs
+++ AliLib.Core/AliLib.CoreScript.cs
@@ -11,10 +11,24 @@ namespace AliLib.Core;
 /// </summary>
 internal class CoreScript : ThunderScript
 {
+    public static bool Initialized { get; private set; } = false;
+
     /// <inheritdoc/>
     public override void ScriptEnable()
     {
         base.ScriptEnable();
+
+        // Multiple AliLibs can link to multiple mods but we only want to init once
+        if (ModManager.loadedMods.SelectMany(mod => mod.assemblies).SelectMany(a => a.GetTypes()).Count(t => t == typeof(CoreScript)) > 1)
+        {
+            if (Initialized)
+                return;
+        }
+
+        if (Initialized)
+            return;
+
+        Initialized = true;
 
         AddressableLibrary.LoadAddressableAssetAttributes();
 
