4453a45b4f0aa7ba7e1c7364f0070586e2322605
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2026-04-08 23:11:02 +1000

    Add `CoroutineRunner`.

---
+++ AliLib.Core/CoroutineRunner.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+namespace AliLib.Core;
+
+/// <summary>
+/// Utility class for running <see cref="Coroutine"/>s from non-monobehaviours.
+/// </summary>
+public class CoroutineRunner : MonoBehaviour
+{
+    /// <summary> Singleton instance. </summary>
+    public static CoroutineRunner Instance
+    {
+        get
+        {
+            if (field == null)
+                field = new GameObject("AliLib.CoroutineRunner").AddComponent<CoroutineRunner>();
+
+            return field;
+        }
+    }
+}
