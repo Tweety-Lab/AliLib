4b9703666e7ce21bf2fc151e1425fc110616120d
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2026-04-16 10:02:48 +1000

    Improve addressables loading.

--- AliLib.Core/Assets/AddressableLibrary.cs
+++ AliLib.Core/Assets/AddressableLibrary.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using ThunderRoad;
+using UnityEngine;
 
 namespace AliLib.Core.Assets;
 
@@ -51,32 +52,101 @@ public static class AddressableLibrary
     /// <summary> Loads assets for all all properties marked with <see cref="AddressableAttribute"/>. </summary>
     public static void LoadAddressableAssetAttributes()
     {
-        // This is a lot of LinQ, we could probably optimize it
-        List<PropertyInfo> properties = ModManager.loadedMods
-            .SelectMany(mod => mod.assemblies)
-            .SelectMany(a => a.GetTypes())
-            .SelectMany(t => t.GetProperties(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic))
-            .Where(p => p.IsDefined(typeof(AddressableAttribute), false))
-            .ToList();
+        var properties = new List<PropertyInfo>();
+
+        foreach (var mod in ModManager.loadedMods)
+        {
+            if (mod?.assemblies == null)
+                continue;
+
+            foreach (var assembly in mod.assemblies)
+            {
+                if (assembly == null)
+                    continue;
+
+                Type[] types;
+
+                try
+                {
+                    types = assembly.GetTypes();
+                }
+                catch (ReflectionTypeLoadException ex)
+                {
+                    types = ex.Types.Where(t => t != null).ToArray();
+                }
+                catch
+                {
+                    continue;
+                }
+
+                foreach (var type in types)
+                {
+                    if (type == null) continue;
+
+                    PropertyInfo[] props;
+
+                    try
+                    {
+                        props = type.GetProperties(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+
+                    foreach (var prop in props)
+                    {
+                        if (prop == null) continue;
+
+                        if (!prop.IsDefined(typeof(AddressableAttribute), false))
+                            continue;
+
+                        properties.Add(prop);
+                    }
+                }
+            }
+        }
+
+        MethodInfo openMethod = typeof(Catalog).GetMethods(BindingFlags.Static | BindingFlags.Public)
+            .FirstOrDefault(m =>
+                m.Name == "LoadAssetAsync" &&
+                m.IsGenericMethodDefinition &&
+                m.GetParameters() is { Length: 3 } p &&
+                p[0].ParameterType == typeof(string) &&
+                p[2].ParameterType == typeof(string)
+            );
+
+        if (openMethod == null)
+            return;
+
+        MethodInfo createCallbackGeneric = typeof(AddressableLibrary)
+            .GetMethod(nameof(CreateTypedCallback), BindingFlags.Static | BindingFlags.NonPublic);
 
         foreach (PropertyInfo property in properties)
         {
-            AddressableAttribute attribute = (AddressableAttribute)property.GetCustomAttributes(typeof(AddressableAttribute), false)[0];
-            Type assetType = property.PropertyType;
-            MethodInfo openMethod = typeof(Catalog).GetMethods(BindingFlags.Static | BindingFlags.Public)
-                .FirstOrDefault(m =>
-                        m.Name == "LoadAssetAsync" &&
-                        m.IsGenericMethodDefinition &&
-                        m.GetParameters() is { } p &&
-                        p.Length == 3 &&
-                        p[0].ParameterType == typeof(string) &&
-                        p[2].ParameterType == typeof(string)
-                );
+            try
+            {
+                var attribute = (AddressableAttribute)property.GetCustomAttributes(typeof(AddressableAttribute), false)[0];
 
-            MethodInfo closedMethod = openMethod.MakeGenericMethod(assetType);
-            MethodInfo createCallbackMethod = typeof(AddressableLibrary).GetMethod(nameof(CreateTypedCallback), BindingFlags.Static | BindingFlags.NonPublic)!.MakeGenericMethod(assetType);
-            object callback = createCallbackMethod.Invoke(null, new object[] { property });
-            closedMethod.Invoke(null, new object[] { attribute.Address, callback, "AliLib.AddressableLibrary" });
+                Type assetType = property.PropertyType;
+
+                MethodInfo closedMethod = openMethod.MakeGenericMethod(assetType);
+                MethodInfo callbackMethod = createCallbackGeneric.MakeGenericMethod(assetType);
+
+                object callback = callbackMethod.Invoke(null, new object[] { property });
+
+                closedMethod.Invoke(null, new object[]
+                {
+                    attribute.Address,
+                    callback,
+                    "AliLib.AddressableLibrary"
+                });
+            }
+            catch
+            {
+                Debug.LogError("Failed to load addressable asset: " + property.Name);
+                continue;
+            }
         }
     }
 
