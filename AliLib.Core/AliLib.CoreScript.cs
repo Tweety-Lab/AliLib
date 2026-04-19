be9a045b5d3da5440d15523d98ae620bf317b999
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2026-04-19 11:38:59 +1000

    Improve initialization logic.

--- AliLib.Core/AliLib.CoreScript.cs
+++ AliLib.Core/AliLib.CoreScript.cs
@@ -18,6 +18,22 @@ internal class CoreScript : ThunderScript
     {
         base.ScriptEnable();
 
+        if (!ShouldInitialize())
+            return;
+
+        Debug.Log($"[AliLib v{Assembly.GetExecutingAssembly().GetName().Version}] Initializing AliLib Core Script...");
+
+        Catalog.LoadAddressableAssetAttributes();
+        Catalog.LoadAddressableComponents();
+
+        ModOptionNomadOnlyAttribute.Setup();
+        ModOptionPCVROnlyAttribute.Setup();
+    }
+
+    /// <summary> Returns whether or not this specific instance of AliLib should be initialized. </summary>
+    public bool ShouldInitialize()
+    {
+
         // Find all loaded AliLib assemblies and pick the most up to date one
         // We do this to ensure that the latest version of this script is used even if a mod depends on an older version
         // Implications of this are... risky
@@ -26,21 +42,21 @@ internal class CoreScript : ThunderScript
         var aliLibAssemblies = ModManager.loadedMods.Where(m => m?.assemblies != null).SelectMany(m => m.assemblies).Where(a => a != null && a.GetName().Name == currentName).OrderByDescending(a => a.GetName().Version).ToList();
 
         if (aliLibAssemblies.Count == 0)
-            return;
+            return false;
 
         Assembly latest = aliLibAssemblies[0];
         Version latestVersion = latest.GetName().Version;
         Version currentVersion = Assembly.GetExecutingAssembly().GetName().Version;
 
         if (currentVersion < latestVersion)
-            return;
+            return false;
 
-        Debug.Log($"[AliLib v{latestVersion}] Initializing AliLib Core Script...");
+        string domainKey = $"AliLib.Initialized.{currentVersion}";
+        if (AppDomain.CurrentDomain.GetData(domainKey) is true)
+            return false;
 
-        Catalog.LoadAddressableAssetAttributes();
-        Catalog.LoadAddressableComponents();
+        AppDomain.CurrentDomain.SetData(domainKey, true);
 
-        ModOptionNomadOnlyAttribute.Setup();
-        ModOptionPCVROnlyAttribute.Setup();
+        return true;
     }
 }
